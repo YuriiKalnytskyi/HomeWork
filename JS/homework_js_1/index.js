216ee913d8774e65479bd61c50a241527fd608bc
YuriiKalnytskyi <yra6920@gmail.com>
2021-03-24 17:14:44 +0200

    homework_html_3 && homework_js_1

---
+++ JS/homework_js_1/index.js
@@ -0,0 +1,469 @@
+//<!---------1-------------------------------------->
+// let a1 = 'hello ';
+// console.log(a1)
+// alert(a1)
+// document.write(a1)
+
+// let a2 = 'owu ';
+// console.log(a2)
+// alert(a2)
+// document.write(a2)
+//
+// let a3 = 'com ';
+// console.log(a3)
+// alert(a3)
+// document.write(a3)
+//
+// let a4 = 'ua ';
+// console.log(a4)
+// alert(a4)
+// document.write(a4)
+//
+// let a5 = '1 ';
+// console.log(a5)
+// alert(a5)
+// document.write(a5)
+//
+// let a6 = '10';
+// console.log(a6)
+// alert(a6)
+// document.write(a6)
+//
+// let a7 = '-999';
+// console.log(a7)
+// alert(a7)
+// document.write(a7)
+//
+// let a8 = '123';
+// console.log(a8)
+// alert(a8)
+// document.write(a8)
+//
+// let a9 = '3.14';
+// console.log(a9)
+// alert(a9)
+// document.write(a9)
+//
+// let a10 = '2.7';
+// console.log(a10)
+// alert(a10)
+//
+// let a11  = '16';
+// console.log(a11)
+// alert(a11)
+// document.write(a11)
+//
+// let a12 = 'true';
+// console.log(a12)
+// alert(a12)
+// document.write(a12)
+//
+// let a13 = 'false';
+// console.log(a13)
+// alert(a13)
+// document.write(a13)
+
+//<!---------2-------------------------------------->
+// let a1 = 'goodbye';
+// console.log(a1)
+// alert(a1)
+// document.write(a1)
+// let a2 = 'or ';
+// console.log(a2)
+// alert(a2)
+// document.write(a2)
+//
+// let a3 = ' you ';
+// console.log(a3)
+// alert(a3)
+// document.write(a3)
+//
+// let a4 = 'ru';
+// console.log(a4)
+// alert(a4)
+// document.write(a4)
+//
+// let a5 = '118 ';
+// console.log(a5)
+// alert(a5)
+// document.write(a5)
+//
+// let a6 = '100000';
+// console.log(a6)
+// alert(a6)
+// document.write(a6)
+//
+// let a7 = '-4';
+// console.log(a7)
+// alert(a7)
+// document.write(a7)
+//
+// let a8 = '843';
+// console.log(a8)
+// alert(a8)
+// document.write(a8)
+//
+// let a9 = '1.14';
+// console.log(a9)
+// alert(a9)
+// document.write(a9)
+//
+// let a10 = '4.9';
+// console.log(a10)
+// alert(a10)
+//
+// let a11  = '4939';
+// console.log(a11)
+// alert(a11)
+// document.write(a11)
+//
+// let a12 = 'false';
+// console.log(a12)
+// alert(a12)
+// document.write(a12)
+//
+// let a13 = 'true';
+// console.log(a13)
+// alert(a13)
+// document.write(a13)
+<!-------------------3----------------------------->
+// let a1 = 19 ;
+// console.log(a1)
+// alert(a1)
+// document.write(a1)
+//
+// let a2 = 2;
+// console.log(a2)
+// alert(a2)
+// document.write(a2)
+//
+// let a3 = 5;
+// console.log(a3)
+// alert(a3)
+// document.write(a3)
+//
+// let a4 = 'world';
+// console.log(a4)
+// alert(a4)
+// document.write(a4)
+//
+// let a5 = 'week';
+// console.log(a5)
+// alert(a5)
+// document.write(a5)
+//
+// let a6 = 'house';
+// console.log(a6)
+// alert(a6)
+// document.write(a6)
+
+<!--------------------4----------------------------->
+// let name = prompt('name')
+// console.log(name)
+// alert(name )
+// document.write(`<h1> name </h1>`)
+//
+// let firstname = prompt('firstname')
+// console.log(firstname)
+// alert(firstname )
+// document.write(`<h1> firstname </h1>`)
+//
+// let surname = prompt('surname')
+// console.log(surname)
+// alert(surname )
+// document.write(`<h1> surname </h1>`)
+//
+//\<!--------------------5----------------------------->
+//
+// let person = name + ' ' + surname  + ' ' + firstname;
+// console.log(person)
+// alert(person )
+// document.write(`<h1>` + person+ `</h1>`);
+
+<!--------------------7----------------------------->
+// let one = +prompt('one')
+// console.log(one)
+//
+// let two = +prompt('two')
+// console.log(two)
+//
+// let three = +prompt('three')
+// console.log(three)
+<!--------------------8----------------------------->
+// let one = parseInt(prompt('one'))
+// console.log(one)
+// let two =parseInt(prompt('two'))
+// console.log(two)
+//
+// let three = parseInt(prompt('three'))
+// console.log(three)
+//
+// let four = parseInt(prompt('four'))
+// console.log(four)
+//
+// let result = one + two + three + four ;
+// console.log(result)
+
+// <!--------------------9----------------------------->
+// let one = parseFloat(prompt('one'));
+// console.log(one);
+//
+// let two =  parseFloat(prompt('two'));
+// console.log(two);
+//
+// let three = parseFloat(prompt('three'));
+// console.log(three);
+//
+// let result = one + two + three  ;
+// console.log(result);
+<!--------------------10----------------------------->
+// let one = Math.round(prompt('one'));
+// console.log(one);
+//
+// let two = Math.round(prompt('two'));
+// console.log(two);
+//
+// let three = Math.round(prompt('three'));
+// console.log(three);
+//
+// let result = one + two + three;
+// console.log(result);
+<!--------------------12----------------------------->
+// let a = 100;
+// console.log(typeof a)
+// let b = '100';
+// console.log(typeof b)
+// let c = true;
+// console.log(typeof c)
+// let d = undefined;
+// console.log(typeof d)
+// <!--------------------16-26 ----------------------------->
+// console.log(5 < 6 )
+// console.log(5 == 6 )
+// console.log(5 === 6 )
+// console.log(5 > 6 )
+// console.log(10 == 10 )
+// console.log(10 <= 10 )
+// console.log(10 < 10 )
+// console.log(10 > 10 )
+// console.log(10 != 10 )
+// console.log(123 == '123' )
+// console.log(123 === '123' )
+
+// <!--------------------Дополнительно----------------------------->
+// console.log(132 > 100 && 45 < 12);//false
+// console.log(34 > 33 && 23 < 90);//true
+// console.log(99 > 100 && 45 > 12);//false
+// console.log(132 > 100 || 45 < 12);//true
+// console.log(111 > 11 || 45 < 111);//true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12));//true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12));//true
+// console.log((132 > 100 && 45 < 12) && (132 > 100 || 45 < 12));//false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12));//true
+// console.log(!!'-1');//true
+// console.log(!!-1);//true
+// console.log(!!'0');//true
+// console.log(!!'null');//true
+// console.log(!!'undefined');//true
+// console.log(!!(3 / 'owu'));//false
+// console.log((111 > 11 || 45 < 111) || !!'0');//true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12));//false
+
+// <!--------------------CLASS----------------------------------------------------->
+
+// <!--------------------1----------------------------->
+
+// let str ='Привет ';
+// console.log(typeof str );
+//
+// let num =123;
+// console.log(typeof num);
+//
+// let flag = true ;
+// console.log(typeof flag );
+//
+// let txt ='true';
+// console.log(typeof  txt);
+
+// <!--------------------2----------------------------->
+// let a1 = 5 + 3 ;
+// console.log(a1) ;
+//
+// let a2 = 5 - 3 ;
+// console.log(a2) ;
+//
+// let a3 = 5 * 3 ;
+// console.log(a3) ;
+//
+// let a4 = 5 / 3 ;
+// console.log(a4) ;
+//
+// let a5 = 5 % 3 ;
+// console.log(a5) ;
+
+// <!--------------------3----------------------------->
+// let a6 = 5 % 3 ;
+// console.log(a6) ;
+//
+// let a7 = 3 % 5 ;
+// console.log(a7) ;
+//
+// let a8 = 5 * '3' ;
+// console.log(a8) ;
+//
+// let a9 = '5 '-  3 ;
+// console.log(a9) ;
+//
+// let a10 = 75 + 'кг' ;
+// console.log(a10) ;
+
+// <!--------------------4----------------------------->
+// let height = 23 ;
+// let width =10 ;
+// let s = 0.5 * height * width ;
+// console.log(s)
+
+// <!--------------------5----------------------------->
+// let heightC = 10 ;
+// let dC = 4 ;
+// let V =  heightC * dC;
+// console.log(V) ;
+
+// <!--------------------6----------------------------->
+// let n = 10 ;
+// let m = 4 ;
+// let k = Math.sqrt((Math.pow(n,2)) * (Math.pow(m,2)));
+// console.log(k) ;
+// let K = Math.sqrt((n**2) * (m**2) );
+// console.log(K) ;
+
+// <!--------------------7----------------------------->
+// let str = 'Hello world';
+// console.log(str);
+// alert(str);
+// document.write(str);
+
+// <!--------------------8----------------------------->
+// alert( "Кальницький Юрій Іванович \n 19 років  \n волейбол ");
+
+// <!--------------------9----------------------------->
+// let str1 = 'Кто ' ;
+// let str2 = 'ти  ' ;
+// let str3 = 'такой? ' ;
+// let concatenation = str1 + ' ' + str2 + ' ' + str3 ;
+// document.write(`<h3>` + concatenation + `</h3>`)
+
+// <!--------------------10----------------------------->
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");//205
+// document.write(str - a + "<br/>");//15
+// document.write(str * "2" + "<br/>");//40
+// document.write(str / 2 + "<br/>");//10
+
+// <!--------------------11----------------------------->
+
+// let a1 = parseInt()"3.14")//число
+// console.log(a1)
+//
+// let a2 = parseInt("-7.875")//число
+// console.log(a2)
+//
+// let a3 =parseInt("435")//число
+// console.log(a3)
+//
+// let a4 = parseInt("Вася")//NaN
+// console.log(a4)
+
+// <!--------------------12----------------------------->
+// let str = prompt("Enter something", "ho-ho" )
+// console.log(str);
+// <!--------------------13----------------------------->
+// let a1 = +prompt('a1') ;
+// let a2 = +prompt('a2') ;
+// let a3 = a1 + a2 ;
+// alert(a3)
+
+// let a1 = parseFloat(prompt('a1')) ;
+// let a2 = parseFloat(prompt('a2')) ;
+// let a3 = a1 + a2 ;
+// alert(a3)
+// <!--------------------14----------------------------->
+// let name = prompt('name');
+// let firstname = prompt('firstname');
+// let surname = prompt('surname');
+// let age = prompt('age');
+// let information = 'Доброго вечора '+ name + ' ' + surname + 'мои поздравления что вам '+ age  ;
+// alert( information );
+//
+// a b c
+// a c b
+// b a c
+// b c a
+// c b a
+// c a b
+
+// <!--------------------ДОП---------------------------------------------->
+
+// <!--------------------1----------------------------->
+
+// let a = prompt(' a');
+// let b = prompt(' b');
+// let c = prompt(' c');
+// if ( a < b && a < c && b < c  ){
+//     console.log( a + ' ' +b+ ' ' +c)
+// }
+// else if ( a < b && a < c < b ){
+//     console.log( a + ' ' +c+ ' ' +b)
+// }
+// else if ( b < a && b < c && a < c){
+//     console.log( b + ' ' + a + ' ' +c)
+// }
+// else if ( b < a && b < c && c < a){
+//     console.log( b + ' '+ c + ' ' +a)
+// }
+// else if ( c < a && c < b && b < a){
+//     console.log( c + ' ' + b + ' ' +a)
+// }
+// else if ( c < a && c < b && a < b){
+//     console.log( c + ' ' + a + ' ' +b)
+// }
+// <!--------------------2----------------------------->
+// let a = prompt(' який колір  світлофора ')
+// if (a === 'зелений' ){
+//     console.log('Йди ! ')
+// }
+// else  if (a === 'жовтий'){
+//     console.log('Зачекай  ! ')
+// }
+// else  if (a === 'червоний '){
+//     console.log('Стій  ! ')
+// }
+// else  {
+//     console.log('Роби що хочеш  ! ')
+// }
+
+// <!--------------------3----------------------------->
+let a = prompt(' який колір  світлофора ')
+let isRoadClear = confirm('Машини є ?' )
+if (a === 'зелений' && isRoadClear === true ){
+    console.log('подожди пока нарушители проедут ')
+}
+else if (a === 'зелений' && isRoadClear === false ){
+    console.log('Йди !')
+}
+else if (a === 'жовтий' && isRoadClear === true ){
+    console.log('Жди  ')
+}
+else if (a === 'жовтий' && isRoadClear === false ) {
+    console.log('Все рано жди ')
+}
+else if (a === 'червоний' && isRoadClear === true ){
+    console.log('Стой и жди ')
+}
+else if (a === 'червоний' && isRoadClear === false ) {
+    console.log('Стой и жди ')
+}
+else {
+    console.log('Делай что хочешь  !')
+}
