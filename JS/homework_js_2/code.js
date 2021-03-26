975bac7fa8a8d70e61b7d61a1b0051af241ac5bc
YuriiKalnytskyi <yra6920@gmail.com>
2021-03-26 22:12:05 +0200

    homework_js_2 ще не повністю

---
+++ JS/homework_js_2/code.js
@@ -0,0 +1,437 @@
+// <---------- Task 1 -------------- >
+// let arr = [1 , 2 ,3, 4 ,5 ] ;
+// console.log(arr)
+
+// let arr = ['hello','home', ' work', ' world' , 'qwerty' ] ;
+// console.log(arr)
+
+// let arr = [1 , 'home' , true,'world',5 ,false  ] ;
+// console.log(arr)
+
+//  let arr =[] ;
+// arr[0]= 1;
+// arr[1]= '1';
+// arr[2]= 'home';
+// arr[3]= true;
+// arr[4]= 'world ';
+// arr[5]= 'qwerty';
+// arr[5]= false;
+// console.log(arr)
+
+// <---------- Task 7 -------------- >
+
+// for ( let i = 0 ; i < 10 ; i++ ){
+//     document.write( '<div> Text  </div>') ;
+//     document.write(`<br/>`);
+// }
+
+// <---------- Task 8 -------------- >
+
+// for ( let i = 0 ; i < 10 ; i++ ){
+//     document.write( '<div> Text: '+i+'  </div>') ;
+//     document.write(`<br/>`);
+// }
+// <---------- Task 9 -------------- >
+
+// let i = 0 ;
+// while ( i < 20){
+//     document.write( '<h1> Text  </h1>') ;
+//     document.write(`<br/>`);
+//     i++
+// }
+
+// <---------- Task 10 -------------- >
+
+// let i = 0 ;
+// while ( i < 20){
+//     document.write( '<h1> Text : '+i+' </h1>') ;
+//     document.write(`<br/>`);
+//     i++
+// }
+
+// <---------- Task 11 -------------- >
+// let  arr = [1,2,3,4,5,6,7,8,9,10] ;
+// for (let i = 0 ; i < arr.length; i++ ){
+//     console.log(arr[i]);
+// }
+// for (let arr1 of arr) {
+//     console.log(arr1);
+// }
+
+// <---------- Task 12 -------------- >
+
+//let arr = ['code', 'home', 'qwerty', 'world', ' whe ', 'false' ,'212', 'who', 'dcdc' , 'true']
+// for (let i = 0 ; i < arr.length; i++){
+//     console.log(arr[i]);
+//}
+// for (let arr1 of arr) {
+//     console.log(arr1);
+//
+// }
+// <---------- Task 13 -------------- >
+
+// let arr = [1, 'home', true, 'world', 5, false , 1212, 'who', 4554 , true];
+// for (let i = 0 ; i < arr.length ; i++ ){
+//     console.log(arr[i] );
+// }
+// for (let arr1 of arr) {
+//     console.log(arr1);
+// }
+
+// // <---------- Task 14 -------------- >
+//
+// let arr = [1, 'home', true, 'world', 5, false , 1212, 'who', 4554 , true];
+// for ( let i = 0 ; i < arr.length ; i++){
+//     if (typeof arr[i]==='boolean'){
+//         console.log(arr[i]);
+//     }
+// }
+// <---------- Task 15 -------------- >
+// let arr = [1, 'home', true, 'world', 5, false , 1212, 'who', 4554 , true];
+// for ( let i = 0 ; i < arr.length ; i++) {
+//     if (typeof arr[i] === 'number') {
+//         console.log(arr[i]);
+//     }
+// }
+// <---------- Task 16 -------------- >
+// let arr = [1, 'home', true, 'world', 5, false , 1212, 'who', 4554 , true];
+// for ( let i = 0 ; i < arr.length ; i++) {
+//     if (typeof arr[i] === 'string') {
+//         console.log(arr[i]);
+//     }
+// }
+// <---------- Task 17 -------------- >
+
+// let arr = [];
+// arr[0] = 1 ;
+// arr[1] = 'home' ;
+// arr[2] = true;
+// arr[3] = 'world';
+// arr[4] =  5 ;
+// arr[5] = false ;
+// arr[6] = 1212;
+// arr[7] = 'who';
+// arr[8] =4554;
+// arr[9] = true;
+// for ( let i = 0 ; i < 10 ;i++){
+//     console.log(arr[i]);
+// }
+// <---------- Task 18 -------------- >
+// for (let i= 0 ; i < 10 ; i++){
+//     console.log(i);
+// }
+
+//<---------- Task 19 -------------- >
+// for (let i= 0 ; i < 100 ; i++){
+//     console.log(i);
+//     document.write(`number : ${i} <br>`);
+// }
+//<---------- Task 20 -------------- >
+
+// for (let i = 0; i < 200; i = i + 2) {
+//     console.log(i);
+//     document.write(`number : ${i} <br>`);
+// }
+
+//<---------- Task 21 -------------- >
+//
+// for (let i = 0; i < 100; i++) {
+//     if(i%2 === 0 ){
+//         console.log(i);
+//         document.write(`number : ${i} <br>`);
+//     }
+// }
+
+//<---------- Task 23 -------------- >
+
+// for (let i = 0; i < 100; i++) {
+//     if(i%2 !== 0 ){
+//         console.log(i);
+//         document.write(`number : ${i} <br>`);
+//     }
+// }
+//<---------- Task 24   -------------- >
+//
+// for (let i=0 ; i < 3 ; i++) {
+//     console.log(`хвилини : ${i} `);
+//     for (let j = 0 ; j < 60; j++){
+//         console.log(j)
+//     }
+// }
+//<---------- Task 25   -------------- >
+
+// for (let i=0 ; i < 3 ; i++){
+//     console.log(`Години : ${i} `);
+//     for (let j=0 ; j < 60 ; j++) {
+//         console.log(`хвилини : ${j} `);
+//         for (let k = 0 ; k < 60; k++){
+//             console.log(k)
+//         }
+//     }
+// }
+
+//<----------  Додатково    -------------- >
+
+// let arr =[ 'a', 'b', 'c'];
+// let arr1 = "" ;
+// for (let i=0 ; i <arr.length; i++){
+//     arr1= arr1 + arr[i]
+// }
+// console.log(arr1);
+
+// let arr =[ 'a', 'b', 'c'];
+// let arr1 = "" ;
+// let i = 0 ;
+// while ( i < arr.length ){
+//     arr1= arr1 + arr[i]
+//     i++
+// }
+// console.log(arr1);
+//
+// let arr =[ 'a', 'b', 'c'];
+// let arr1 = "" ;
+// let i = 0 ;
+// for (let arr2 of arr) {
+//     arr1= arr1 + arr2;
+// }
+// console.log(arr1);
+
+
+// let arr =[ 'a', 'b', 'c'];
+// let arr1 = "" ;
+// let i = 0 ;
+// for (let arr2 in arr) {
+//     arr1= arr1 + arr[arr2];
+// }
+// console.log(arr1);
+
+//<----------  37------------------ >
+
+// let arr = ['a', 'b', 'c'];
+// for ( let i = 0 ; i < 4 ; i++){
+//     arr.push(i)
+// }
+// console.log(arr);
+
+//<----------  38------------------ >
+
+// let arr =[1, 2, 3];
+// for (let i = arr.length-1 ; i >= 0 ; i-- ){
+//     console.log(arr[i]);
+// }
+
+//<----------  39------------------ >
+// let arr = [1, 2, 3];
+// for ( let i = 4 ; i <= 6  ; i++){
+//     arr.push(i)
+// }
+// console.log(arr);
+
+// //<----------  40------------------ >
+// let arr = [1, 2, 3];
+// for ( let i = 6 ; i >= 4  ; i--){
+//     arr.unshift(i);
+// }
+// console.log(arr);
+// //<----------  41------------------ >
+
+// let arr = ['js', 'css', 'jq'];
+// let arr1 = arr.shift();
+// console.log( arr1);
+
+// //<----------  42------------------ >
+
+// let arr = ['js', 'css', 'jq'];
+// let arr1 = arr.pop();
+// console.log( arr1);
+
+// //<----------  43------------------ >
+
+// let arr = [1, 2, 3, 4, 5] ;
+// console.log(arr.slice(3));
+
+// //<----------  44------------------ >
+
+// let arr = [1, 2, 3, 4, 5] ;
+// console.log(arr.slice(0,2));
+
+// //<----------  45------------------ >
+
+// const arr = [1, 2, 3, 4, 5] ;
+// console.log(arr.splice(1,2));
+// console.log(arr);
+
+// //<----------  46------------------ >
+
+// const arr = [1, 2, 3, 4, 5] ;
+// arr.splice(3,0, 'a','b','c');
+// console.log(arr);
+
+// <----------  47  ------------------ >????????????????????????????????????????????????????????????????????????????????
+// Дан масив [1, 2, 3, 4, 5]. За допомогою методу/функції splice зробіть з нього масив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+// const arr = [1, 2, 3, 4, 5] ;
+// arr.splice(1, 0, 'a','b' );
+// arr.splice(3, 0, 'c', );
+// arr.splice(4, 0, 'e', );
+// console.log(arr);
+
+// //<----------  48 ------------------ >
+
+// for (let i = 0 ; i<10 ; i++){
+//     if(i%2 ===0){
+//         console.log(i);
+//     }
+// }
+
+// //<----------  49 ------------------ >
+// let arr = [2,5,6,1,5,1,5,7,2,9];
+// let arr1 = [];
+// for (let i = 0 ; i<arr.length ; i++){
+//     arr1.push(arr[i])
+//
+// }
+// console.log(arr1);
+//<----------  50 ------------------ > ??????????????????????????
+//
+// let arr = [2,5,6,1,5,1,5,7,2,9];
+// let arr1 = [];
+// for (let i = 0 ; i < arr.length ; i++){
+//     arr1= [arr1 + arr[i]];
+// }
+// console.log(arr1);
+
+// //<----------  56 ------------------ >
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// let i=0;
+// while (i<arr.length){
+//     console.log(arr[i]);
+//     i++
+// }
+
+// //<----------  57 ------------------ >
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// for (let i =0 ; i < arr.length ; i++){
+//     console.log(arr[i]);
+// }
+
+// //<----------  58 ------------------ >
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// let i=0;
+// while (i<arr.length-1){
+//     i++
+//     if (i%2 === 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// //<----------  59 ------------------ >
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// for (let i =0 ; i < arr.length ; i++){
+//     if (i%2 === 0){
+//         console.log(arr[i]);
+//     }
+// }
+// //<----------  60 ------------------ >
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// let i=0;
+// while (i<arr.length-1){
+//     i++
+//     if (i%2 !== 0){
+//         console.log(arr[i]);
+//     }
+// }
+// //<----------  61 ------------------ >
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// for (let i =0 ; i < arr.length ; i++){
+//     if (i%2 !== 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+//<----------  62 ------------------ >
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// console.log(arr);
+// for (let i =0 ; i < arr.length ; i++){
+//     if (arr[i]%3 === 0){
+//         arr[i]='okten'
+//     }
+// }
+// console.log(arr);
+
+// //<----------  63 ------------------ >
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// for (let i = arr.length-1 ; i >=0;i--){
+//     console.log(arr[i]);
+// }
+
+// //<----------  64 ------------------ >
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// let i=arr.length-1;
+// while (i>=0){
+//     console.log(arr[i]);
+//     i--
+// }
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// for (let i = arr.length-1 ; i >=0  ; i--){
+//     console.log(arr[i]);
+// }
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// let i=arr.length;
+// while (i >=0){
+//     i--
+//     if (i%2 === 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// for (let i = arr.length-1; i >=0  ; i--){
+//     if (i%2 === 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// let i=arr.length;
+// while (i >=0){
+//     i--
+//     if (i%2 !== 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+// for (let i = arr.length ; i >=0  ; i--){
+//     if (i%2 !== 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// //<----------  67 ------------------ >
+
+// let arr=[];
+// for (let i = 0 ; i<100; i++){
+//     arr.push(i)
+//     if(arr[i] %2 === 0){
+//         console.log(arr[i])
+//     }
+// }
+// //<----------  68 ------------------ >
+
+// let arr=[];
+// for (let i = 0 ; i<100; i++){
+//     arr.push(i)
+//     if(arr[i] %2 !== 0){
+//         console.log(arr[i])
+//     }
+// }
